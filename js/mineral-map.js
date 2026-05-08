7583bee47c1d78e4d7c347ef1c265cbce1c38776
Usman Saeed <abenxio88@gmail.com>
2026-05-08 19:22:51 +0500

    commit

--- js/mineral-map.js
+++ js/mineral-map.js
@@ -130,7 +130,7 @@ function init(){
 
   // Build SVG
   var svg = document.createElementNS('http://www.w3.org/2000/svg','svg');
-  svg.setAttribute('viewBox','0 0 1100 750');
+  svg.setAttribute('viewBox','50 30 1020 680');
   svg.setAttribute('preserveAspectRatio','xMidYMid meet');
   svg.style.cssText = 'width:100%;height:100%;display:block;background:#111;';
 
